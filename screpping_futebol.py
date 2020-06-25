a34389e38c9d002da2b03d4962be3841643b6088
izaias <zambelestar@gmail.com>
2020-06-25 20:34:53 -0300

    rebild adcionando copa Sulamericana e rebaixamento do campeonato Brasileiro

--- screpping_futebol.py
+++ screpping_futebol.py
@@ -2,25 +2,27 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import json
-''''''
+
+
 page = requests.get("https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a")
-''''''
+#
 soup = BeautifulSoup(page.content, 'html.parser')
 tabela = soup.find_all(name='table')
-''''''
+
+
 df_full = pd.read_html(str(tabela))[0].head(20)
 df = df_full[['Posição', 'PTS', 'J', 'V', 'E', 'D', 'GP', 'GC', 'SG', 'CA', 'CV', '%']]
-'''OPCIONAL'''
 '''df.columns = ['Pos', 'PTS', 'J', 'V', 'E', 'D', 'GP', 'GC', 'SG', 'CA', 'CV', '%']'''
-''''''
+
 tabela_brasileiro = {}
 tabela_brasileiro['Rank_Brasileiro'] = df.to_dict('records')
 js = json.dumps(tabela_brasileiro, ensure_ascii=False, indent=2)
 fp = open('tabela.json','w', encoding='utf-8')
 fp.write(js)
 fp.close()
-''''''
-print('\n',df)
+
+print('\n','Campeonato Brasileiro:')
+print('\n',df,'\n')
 
 with open('tabela.json', 'r') as fin:
     a = fin.readlines()[3]
@@ -57,8 +59,23 @@ with open('tabela.json', 'r') as fin:
 
 with open('tabela.json', 'r') as fin:
     m = fin.readlines()[157]
+
+with open('tabela.json', 'r') as fin:
+    n = fin.readlines()[227]
+
+with open('tabela.json', 'r') as fin:
+    o = fin.readlines()[241]
+
+with open('tabela.json', 'r') as fin:
+    p = fin.readlines()[255]
+
+with open('tabela.json', 'r') as fin:
+    q = fin.readlines()[269]
+
 ''''''
-print("Copa libertadores:")
+print('Copa libertadores:')
 print('\n', a, b, c, d, e, f)
-print("CopaSulamericana:")
+print('CopaSulamericana:')
 print('\n', g, h, i, j, l, m)
+print('Rebaixados do campeonato Brasileiro:')
+print('\n', n, o, p, q)
