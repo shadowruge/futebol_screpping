039bcd670c468382980c016c5cf50ca0788af6e5
izaias <zambelestar@gmail.com>
2020-06-25 22:30:47 -0300

    Update screpping_futebol.py

--- screpping_futebol.py
+++ screpping_futebol.py
@@ -3,27 +3,27 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-
+''''''
 page = requests.get("https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a")
-#
+''''''
 soup = BeautifulSoup(page.content, 'html.parser')
 tabela = soup.find_all(name='table')
-
-
+''''''
 df_full = pd.read_html(str(tabela))[0].head(20)
 df = df_full[['Posição', 'PTS', 'J', 'V', 'E', 'D', 'GP', 'GC', 'SG', 'CA', 'CV', '%']]
+'''opcional'''
 '''df.columns = ['Pos', 'PTS', 'J', 'V', 'E', 'D', 'GP', 'GC', 'SG', 'CA', 'CV', '%']'''
-
+''''''
 tabela_brasileiro = {}
 tabela_brasileiro['Rank_Brasileiro'] = df.to_dict('records')
 js = json.dumps(tabela_brasileiro, ensure_ascii=False, indent=2)
 fp = open('tabela.json','w', encoding='utf-8')
 fp.write(js)
 fp.close()
-
+''''''
 print('\n','Campeonato Brasileiro:')
 print('\n',df,'\n')
-
+''''''
 with open('tabela.json', 'r') as fin:
     a = fin.readlines()[3]
 
@@ -71,7 +71,6 @@ with open('tabela.json', 'r') as fin:
 
 with open('tabela.json', 'r') as fin:
     q = fin.readlines()[269]
-
 ''''''
 print('Copa libertadores:')
 print('\n', a, b, c, d, e, f)
