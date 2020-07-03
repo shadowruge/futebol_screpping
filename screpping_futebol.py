d3ee4b3eb5b18318300d0ed03cb9f3dc0a127b2f
izaias <zambelestar@gmail.com>
2020-07-02 22:12:13 -0300

    rebiuld py

--- screpping_futebol.py
+++ screpping_futebol.py
@@ -1,29 +1,26 @@
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import json
+from main import*
 
-''''''
+
 page = requests.get("https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a")
-''''''
+#
 soup = BeautifulSoup(page.content, 'html.parser')
 tabela = soup.find_all(name='table')
-''''''
+
+
 df_full = pd.read_html(str(tabela))[0].head(20)
-df = df_full[['Posição', 'PTS', 'J', 'V', 'E', 'D', 'GP', 'GC', 'SG', 'CA', 'CV', '%']]
-'''opcional'''
-'''df.columns = ['Pos', 'PTS', 'J', 'V', 'E', 'D', 'GP', 'GC', 'SG', 'CA', 'CV', '%']'''
-''''''
+df = df_full[['Posição', 'PTS', 'J', 'V', 'E', 'D', 'GP', 'GC', 'SG', 'CA', 'CV', '%']] #se esta linha for mudada altera o contexto do arquivo de saida .json
+df.columns = ['Pos', 'PTS', 'J', 'V', 'E', 'D', 'GP', 'GC', 'SG', 'CA', 'CV', 'Porcentagem']
+
 tabela_brasileiro = {}
-tabela_brasileiro['Rank_Brasileiro'] = df.to_dict('records')
+tabela_brasileiro[''] = df.to_dict('records')
 js = json.dumps(tabela_brasileiro, ensure_ascii=False, indent=2)
 fp = open('tabela.json','w', encoding='utf-8')
 fp.write(js)
 fp.close()
-''''''
+
 print('\n','Campeonato Brasileiro:')
 print('\n',df,'\n')
-''''''
+
 with open('tabela.json', 'r') as fin:
     a = fin.readlines()[3]
 
@@ -71,6 +68,7 @@ with open('tabela.json', 'r') as fin:
 
 with open('tabela.json', 'r') as fin:
     q = fin.readlines()[269]
+
 ''''''
 print('Copa libertadores:')
 print('\n', a, b, c, d, e, f)
