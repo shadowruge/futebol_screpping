6f2b7a44bc13c9d7caf97b7f665b5334bae658ed
izaias <izaias@iclusiva.local>
2020-07-11 17:30:56 -0300

    rebiuld

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Title</title>
+    <title>Tabela do Campeonato Brasileiro</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <!-- Bootstrap CSS -->
@@ -11,50 +11,73 @@
 
 <body>
     
-    <section>
-        <div class="conteiner">
-            <div class="table table-striped table-responsive">
-                <?PHP
-                $arquivo = "tabela.json";
+    <div class="conteiner">
+    
+        <nav>
+            <ul class="menu">
+                <li>Libertadores: 1º, 2º, 3º, 4º, 5º, 6º</li>
+                <li>Fase de grupos: 1º, 2º, 3º, 4º'</li>
+                <li>Fase preliminar: 5º, 6º'</li>
+                <li>Copa Sulamericana: 7º, 8º, 9º, 10º, 11º, 12º</li>
+                <li>Rebaixamento Brasileiro: 17º, 18º, 19º, 20º</li>
+            </ul>
+        </nav>
+        
+        <section>
+            
+            <?PHP
+            $arquivo = "tabela.json";
 
-                $info = file_get_contents($arquivo);
+            $info = file_get_contents($arquivo);
 
-                $lendo = json_decode($info);
+            $lendo = json_decode($info);
 
-                foreach ($lendo->Rank as $campo) {
-                    print "<article>";
-                    print "<br>Pos:  <br/>"
-                        . $campo->Pos;
-                    print "<th>  |  PTS:  </th>"
-                        . $campo->PTS;
-                    print "<th>  |  J:  </th>"
-                        . $campo->J;
-                    print "<th>  |  V:  </th>"
-                        . $campo->V;
-                    print "<th>  |  E:  </th>"
-                        . $campo->E;
-                    print "<th>  |  D:  </th>"
-                        . $campo->D;
-                    print "<th>   |  GP:  </th>"
-                        . $campo->GP;
-                    print "<th>   |  GC:  </th>"
-                        . $campo->GC;
-                    print "<th>   |  SG:  </th>"
-                        . $campo->SG;
-                    print "<th>   |  CA:  </th>"
-                        . $campo->CA;
-                    print "<th>   |  CV:  </th>"
-                        . $campo->CV;
-                    print "<th>   |   Porcentagem:   </th>"
-                        . $campo->Porcentagem;
-                    print "</article>";
-                }
-                ?>
+            $table .= '<table border="1">';
+            $table .= '<thead>';
+            $table .= '<td>Pos</td>';
+            $table .= '<td>PTS</td>';
+            $table .= '<td>J</td>';
+            $table .= '<td>V</td>';
+            $table .= '<td>E</td>';
+            $table .= '<td>D</td>';
+            $table .= '<td>GP</td>';
+            $table .= '<td>GC</td>';
+            $table .= '<td>SG</td>';
+            $table .= '<td>CA</td>';
+            $table .= '<td>CV</td>';
+            $table .= '<td>Porcentagem</td>';
+            $table .= '</thead>';
+            $table .= '<tbody>';
+            foreach ($lendo->Rank as $campo) {
 
-                </table>
-            </div>
-        </div>
-    </section>
+                
+                $table .= "<td>{$campo->Pos}</td>";
+                $table .= "<td>{$campo->PTS}</td>";
+                $table .= "<td>{$campo->J}</td>";
+                $table .= "<td>{$campo->V}</td>";
+                $table .= "<td>{$campo->E}</td>";
+                $table .= "<td>{$campo->D}</td>";
+                $table .= "<td>{$campo->GP}</td>";
+                $table .= "<td>{$campo->GC}</td>";
+                $table .= "<td>{$campo->SG}</td>";
+                $table .= "<td>{$campo->CA}</td>";
+                $table .= "<td>{$campo->CV}</td>";
+                $table .= "<td>{$campo->Porcentagem}</td></tr>";
+                
+            }
+            // fecahamento do html
+           
+            $table .= '</tbody>';
+            $table .= '</table>';
+
+            // exibição na tela
+            echo $table;
+            ?>
+        </section>
+
+    </div>
+
+
 
 </body>
 
