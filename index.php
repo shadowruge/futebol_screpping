ead3dab033cb8c74095a878f346579291a83ec2c
izaias <zambelestar@gmail.com>
2020-08-20 21:35:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,314 +1,42 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>Tabela do Campeonato Brasileiro</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
 <body>
+    <?php
 
-    <div class="conteiner">
-
-        <nav>
-            <ul class="menu">
-                <li>Libertadores: 1º, 2º, 3º, 4º, 5º, 6º</li>
-                <li>Fase de grupos: 1º, 2º, 3º, 4º'</li>
-                <li>Fase preliminar: 5º, 6º'</li>
-                <li>Copa Sulamericana: 7º, 8º, 9º, 10º, 11º, 12º</li>
-                <li>Rebaixamento Brasileiro: 17º, 18º, 19º, 20º</li>
-            </ul>
-        </nav>
-
-        <section>
-
-            <table border="1">
-                <thead>
-                    <td>Pos</td>
-                    <td>PTS</td>
-                    <td>J</td>
-                    <td>V</td>
-                    <td>E</td>
-                    <td>D</td>
-                    <td>GP</td>
-                    <td>GC</td>
-                    <td>SG</td>
-                    <td>CA</td>
-                    <td>CV</td>
-                    <td>Porcentagem</td>
-                </thead>
-                <tbody>
-                    <td>1º Athletico Paranaense - PR</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>2º Atlético - GO</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>3º Atlético - MG</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>4º Bahia - BA</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>5º Botafogo - RJ</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>6º Bragantino - SP</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>7º Ceará - CE</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>8º Corinthians - SP</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>9º Coritiba - PR</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>10º Flamengo - RJ</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>11º Fluminense - RJ</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>12º Fortaleza - CE</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>13º Goiás - GO</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>14º Grêmio - RS</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>15º Internacional - RS</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>16º Palmeiras - SP</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>17º Santos - SP</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>18º São Paulo - SP</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>19º Sport - PE</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                    <td>20º Vasco da Gama - RJ</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    <td>0</td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
-
-    </div>
-
-
-
+    $json = "tabela.json";
+    $info = file_get_contents($json);
+    $lendo = json_decode($info);
+    foreach ($lendo->Rank as $row) {
+        print "<article>";
+        print "<br /><b>Pos:</b> " .$row->Pos;
+        print "<br /><b>PTS:</b> " .$row->PTS;
+        print "<br /><b>J:</b> " .$row->J;
+        print "<br /><b>V:</b> " .$row->V;
+        print "<br /><b>E:</b> " .$row->E;
+        print "<br /><b>D:</b> " .$row->D;
+        print "<br /><b>GP:</b> " .$row->GP;
+        print "<br /><b>GC:</b> " .$row->GC;
+        print "<br /><b>SG:</b> " .$row->SG;
+        print "<br /><b>CA:</b> " .$row->CA;
+        print "<br /><b>CV:</b> " .$row->CV;
+        print "<br /><b>Porcentagem:</b> " .$row->Porcentagem;
+        print "</article>";
+        
+    }
+    ?>
+<p>Libertadores: 1º, 2º, 3º, 4º, 5º, 6º<br/>
+Fase de grupos: 1º, 2º, 3º, 4º'<br/>
+Fase preliminar: 5º, 6º'<br/>
+Copa Sulamericana: 7º, 8º, 9º, 10º, 11º, 12º<br/>
+Rebaixamento Brasileiro: 17º, 18º, 19º, 20º<br/>
+</P>         
 </body>
 
 </html>
