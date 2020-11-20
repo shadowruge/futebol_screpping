25aee47091548434408a81f25dd65f72ff09deb0
izaias <zambelestar@gmail.com>
2020-11-20 09:30:12 -0300

    Updateindex.php

--- index.php
+++ index.php
@@ -4,39 +4,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/futebol_screpping/css/estilo.css" type="text/css" />
+    <title>Brasileirão</title>
 </head>
 
 <body>
-    <?php
+    <header>
+        <nav>
+            <ul class="menu">
+                <li>Libertadores: 1º, 2º, 3º, 4º, 5º, 6º</li>
+                <li>Fase de grupos: 1º, 2º, 3º, 4º'</li>
+                <li>Fase preliminar: 5º, 6º'</li>
+                <li>Copa Sulamericana: 7º, 8º, 9º, 10º, 11º, 12º</li>
+                <li>Rebaixamento Brasileiro: 17º, 18º, 19º, 20º</li>
+            </ul>
+        </nav>
+    </header>
 
-    $json = "tabela.json";
-    $info = file_get_contents($json);
-    $lendo = json_decode($info);
-    foreach ($lendo->Rank as $row) {
-        print "<article>";
-        print "<br /><b>Pos:</b> " .$row->Pos;
-        print "<br /><b>PTS:</b> " .$row->PTS;
-        print "<br /><b>J:</b> " .$row->J;
-        print "<br /><b>V:</b> " .$row->V;
-        print "<br /><b>E:</b> " .$row->E;
-        print "<br /><b>D:</b> " .$row->D;
-        print "<br /><b>GP:</b> " .$row->GP;
-        print "<br /><b>GC:</b> " .$row->GC;
-        print "<br /><b>SG:</b> " .$row->SG;
-        print "<br /><b>CA:</b> " .$row->CA;
-        print "<br /><b>CV:</b> " .$row->CV;
-        print "<br /><b>Porcentagem:</b> " .$row->Porcentagem;
-        print "</article>";
-        
-    }
-    ?>
-<p>Libertadores: 1º, 2º, 3º, 4º, 5º, 6º<br/>
-Fase de grupos: 1º, 2º, 3º, 4º'<br/>
-Fase preliminar: 5º, 6º'<br/>
-Copa Sulamericana: 7º, 8º, 9º, 10º, 11º, 12º<br/>
-Rebaixamento Brasileiro: 17º, 18º, 19º, 20º<br/>
-</P>         
+    <div class="conteiner">
+        <?php
+        $json = "tabela.json";
+        $info = file_get_contents($json);
+        $lendo = json_decode($info);
+        foreach ($lendo->Rank as $row) {
+
+            echo "<table border='1'>";
+            echo "<tr>";
+            echo "<td>Pos<br>$row->Pos</td>";
+            echo "<td>PT<br>$row->PTS</td>";
+            echo "<td>J<br>$row->J</td>";
+            echo "<td>V<br>$row->V</td>";
+            echo "<td>E<br>$row->E</td>";
+            echo "<td>D<br>$row->D</td>";
+            echo "<td>GP<br>$row->GP</td>";
+            echo "<td>GC<br>$row->GC</td>";
+            echo "<td>SG<br>$row->SG</td>";
+            echo "<td>CA<br>$row->CA</td>";
+            echo "<td>CV<br>$row->CV</td>";
+            echo "<td>Porc<br>$row->Porcentagem</td>";
+            echo "</tr>";
+        }
+
+        ?>
+    </div>
+
+
+
 </body>
 
 </html>
